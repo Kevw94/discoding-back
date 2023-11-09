308c0ee9eb19392b037e4784f66573b1d03eceda
Kevw94 <pinerokevin@icloud.com>
2023-11-09 11:46:13 +0100

    implement create server

--- src/servers/servers.controller.ts
+++ src/servers/servers.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { JwtAuthGuard } from '@/common/guards/jwt-auth.guard';
+import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { ServersService } from './servers.service';
+import { Response } from 'express';
+import { JwtRequest } from '@/auth/interfaces/jwt.interface';
+import { CreateServerDTO } from './dto/servers.dto';
 
 @Controller('servers')
-export class ServersController {}
+@UseGuards(JwtAuthGuard)
+export class ServersController {
+	constructor(private readonly serversService: ServersService) {}
+
+	@Get('')
+	async getServers(@Req() req: JwtRequest, @Res() res: Response) {
+		const serversMe = await this.serversService.getAllServersMe(req.user.userId)
+		return res.status(200).json({ status: 'ok', servers: serversMe});
+	}
+
+	@Post('create')
+	async createServer(@Req() req: JwtRequest, @Res() res: Response, @Body() body: CreateServerDTO) {
+		const newServer = await this.serversService.createServer(req.user.userId, body)
+		return res.status(201).json({ status: 'ok', server: newServer});
+	}
+
+}
--- src/servers/servers.service.ts
+++ src/servers/servers.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import { ServersRepository } from './servers.repository';
+import { CreateServerDTO } from './dto/servers.dto';
+import { Server } from './interfaces/servers.interface';
 
 @Injectable()
-export class ServersService {}
+export class ServersService {
+	constructor(
+		@Inject(forwardRef(() => ServersRepository))
+		private serversRepository: ServersRepository,
+	) { }
+
+	async getAllServersMe(userId: string) {
+		const serversMe = await this.serversRepository.findMany({owner_id: userId})
+		return serversMe
+	}
+
+	async createServer(userId: string, payload: Server) {
+		payload.owner_id = userId
+		payload.created_at = new Date();
+
+		const serverCreated = await this.serversRepository.createServer(payload);
+		const serverToRetreive = await this.serversRepository.findOne({_id: serverCreated.insertedId})
+		return serverToRetreive
+	}
+}
