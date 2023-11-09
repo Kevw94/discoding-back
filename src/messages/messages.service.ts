dd371ae95298d6245c001cda14d06469e13f5942
Kevw94 <pinerokevin@icloud.com>
2023-11-09 16:53:41 +0100

    Implement create message

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import { MessagesRepository } from './messages.repository';
+import { Message } from './interfaces/messages.interface';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+	constructor(
+		@Inject(forwardRef(() => MessagesRepository))
+		private messagesRepository: MessagesRepository,
+	) { }
+
+	async createMessage(userId: string, payload: Message) {
+		payload.user_id = userId
+		payload.created_at = new Date()
+
+		const newMessage = await this.messagesRepository.createMessage(payload)
+		const messageToRetreive = await this.messagesRepository.findOne({_id: newMessage.insertedId})
+
+		return messageToRetreive
+	}
+}
