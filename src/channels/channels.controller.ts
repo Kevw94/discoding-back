6920b671381b594c123c71003bad696a04a856f5
Kevw94 <pinerokevin@icloud.com>
2023-11-09 17:40:45 +0100

    Implement channels module

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { ChannelsService } from './channels.service';
+import { JwtRequest } from '@/auth/interfaces/jwt.interface';
+import { JwtAuthGuard } from '@/common/guards/jwt-auth.guard';
+import { Response } from 'express';
+import { CreateChannelDTO } from './dto/channels.dto';
 
 @Controller('channels')
-export class ChannelsController {}
+@UseGuards(JwtAuthGuard)
+export class ChannelsController {
+	constructor(private readonly channelsService: ChannelsService) {}
+
+	@Get(":serverId")
+	async getChannelsByServerId(@Param('serverId') serverId: string,@Res() res: Response) {
+		const channels = await this.channelsService.getChannelsByServerId(serverId)
+		return res.status(201).json({ status: 'ok', newChannel: channels});
+	}
+
+	@Post("create")
+	async createMessage(@Res() res: Response, @Body() body: CreateChannelDTO) {
+		const newChannel = await this.channelsService.createChannel(body)
+		return res.status(201).json({ status: 'ok', newChannel: newChannel});
+	}
+}
--- src/channels/channels.service.ts
+++ src/channels/channels.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import { ChannelsRepository } from './channels.repository';
+import { Channel } from './interfaces/channels.interface';
 
 @Injectable()
-export class ChannelsService {}
+export class ChannelsService {
+	constructor(
+		@Inject(forwardRef(() => ChannelsRepository))
+		private channelssRepository: ChannelsRepository,
+	) { }
+
+	async createChannel(payload: Channel) {
+		const createChannel = await this.channelssRepository.createChannel(payload)
+		const channelToRetreive = await this.channelssRepository.findOne({_id: createChannel.insertedId})
+		return channelToRetreive
+	}
+
+	async getChannelsByServerId(serverId: string) {
+		const channels = await this.channelssRepository.findMany({server_id: serverId})
+
+		return channels
+	}
+}
