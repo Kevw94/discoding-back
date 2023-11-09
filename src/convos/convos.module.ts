1e280984e4fd0e7948ede06017331fee8e9730a5
Kevw94 <pinerokevin@icloud.com>
2023-11-09 14:51:52 +0100

    Implement create new conv

--- src/convos/convos.module.ts
+++ src/convos/convos.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConvosController } from './convos.controller';
 import { ConvosService } from './convos.service';
+import { ConvosRepository } from './convos.repository';
+import { DatabaseModule } from '@/external-module/database/mongo.module';
 
 @Module({
-  controllers: [ConvosController],
-  providers: [ConvosService]
+	imports: [DatabaseModule],
+	controllers: [ConvosController],
+	providers: [ConvosService, ConvosRepository]
 })
-export class ConvosModule {}
+export class ConvosModule { }
--- src/convos/convos.service.ts
+++ src/convos/convos.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import { ConvosRepository } from './convos.repository';
+import { Convo } from './interfaces/convos.interface';
 
 @Injectable()
-export class ConvosService {}
+export class ConvosService {
+	constructor(
+		@Inject(forwardRef(() => ConvosRepository))
+		private convosRepository: ConvosRepository,
+	) { }
+
+	async createConvo(userId: string, payload: Convo) {
+		payload.user_one = userId
+		payload.updated_at = new Date()
+		payload.content = []
+
+		const newConv = await this.convosRepository.createConvo(payload)
+		const convToRetreive = await this.convosRepository.findOne({_id: newConv.insertedId})
+		return convToRetreive
+	}
+}
